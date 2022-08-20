df18337928c6380dfea05e88f67b54daa79ae008
Thanh Le <thanhlevan0203@gmail.com>
2022-08-20 09:17:27 -0700

    Created Login form

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    </head>
+    <body>
+        
+        <h1>Login</h1>
+
+        <form>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" />
+
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" />
+
+            <button>Log In</button>
+        </form>
+    </body>
+
+</html>
