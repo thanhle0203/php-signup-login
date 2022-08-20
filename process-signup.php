1546055bd96f6ac7807a931abb55856d8709a006
Thanh Le <thanhlevan0203@gmail.com>
2022-08-20 11:50:33 -0700

    completed application

--- process-signup.php
+++ process-signup.php
@@ -1,12 +1,10 @@
 <?php
-session_start();
-require 'database.php';
 
 if (empty($_POST["name"])) {
     die("Name is required");
 }
 
-if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
     die("Valid email is required");
 }
 
@@ -14,11 +12,11 @@ if (strlen($_POST["password"]) < 8) {
     die("Password must be at least 8 characters");
 }
 
-if (!preg_match("/[a-z]/i", $_POST["password"])) {
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain at least one letter");
 }
 
-if (!preg_match("/[0-9]/", $_POST["password"])) {
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
 
@@ -30,27 +28,30 @@ $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
 
-$sql = "INSERT INTO user (name, email, password_hash) 
-        VALUE (?, ?, ?)";
+$sql = "INSERT INTO user (name, email, password_hash)
+        VALUES (?, ?, ?)";
+        
 $stmt = $mysqli->stmt_init();
 
-if (! $stmt->prepare($sql)) {
+if ( ! $stmt->prepare($sql)) {
     die("SQL error: " . $mysqli->error);
 }
 
-$stmt->bind_param("sss", 
-                    $_POST["name"], 
-                    $_POST["email"], 
-                    $password_hash);
-
+$stmt->bind_param("sss",
+                  $_POST["name"],
+                  $_POST["email"],
+                  $password_hash);
+                  
 if ($stmt->execute()) {
+
     header("Location: signup-success.html");
     exit;
+    
 } else {
+    
     if ($mysqli->errno === 1062) {
         die("email already taken");
     } else {
         die($mysqli->error . " " . $mysqli->errno);
     }
 }
-
